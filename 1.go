d6a228a82b527edc5e324c17579b0f9751c3ed6e
Andrii Kostenko <andrii@short.io>
2021-10-02 22:29:07 +0300

    parallelization

--- 1.go
+++ 1.go
@@ -2,15 +2,15 @@ package main
 import (
     "fmt"
     "time"
+    "runtime"
     "lukechampine.com/uint128"
 )
 
-func main() {
-    iterated := uint128.From64(^uint64(0))
-    printLimit := 0;
-    t := time.Now()
-    for {
-        x := iterated
+const THREAD_CHUNK = 10_000_000
+
+func checkNumber(iterated uint128.Uint128, guard chan struct{}) {
+    for i := iterated; i.Cmp(iterated.Add64(10_000_000)) == -1; i = i.Add64(1) {
+        x := i
         for {
             if (x.Cmp(iterated) == -1) {
                 break;
@@ -20,43 +20,25 @@ func main() {
             }
             x = x.Rsh(1)
         }
-        iterated = iterated.Add64(1)
-        printLimit += 1;
-        if (printLimit == 100_000_000) {
+    }
+    <- guard
+}
+
+func main() {
+    iterated := uint128.From64(^uint64(0))
+    printLimit := 0;
+    t := time.Now()
+    guard := make(chan struct{}, runtime.NumCPU())
+
+    for {
+        guard <- struct {}{}
+        go checkNumber(iterated, guard)
+        iterated = iterated.Add64(THREAD_CHUNK)
+        printLimit += THREAD_CHUNK;
+        if (printLimit == 1000_000_000) {
             fmt.Println(iterated, time.Now().Sub(t), time.Now())
             t = time.Now()
             printLimit = 0;
         }
     }
-    // = 9789690303392599179035;
-    /*
-    let c = 9789690303392599179035n;
-    let i = 0;
-    while (true) {
-        c = c + BigInt((Math.random() * 4000000000).toFixed());
-        x = c;
-        i ++;
-        if (i % 500000 === 0)
-            console.log(x, new Date());
-        let l = 0;
-        while (true) {
-            if (x <= MAX_CHECKED) {
-                break;
-            }
-            l ++;
-            if (x === 0n) {
-                console.log('0!!!!');
-                process.exit(1);
-            }
-            if (x % 2n === 1n) {
-                x = x * 3n + 1n;
-            }
-            if (x % 2n === 0n) {
-                x = x / 2n;
-            }
-            if (l > 50000) {
-                console.log('INF!', c, x, l)
-            }
-        }
-    }*/
 }
